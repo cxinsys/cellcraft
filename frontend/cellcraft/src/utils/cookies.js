288e6e6e03e5e5747baa5a265a90a719f0d2117a
shindongmin11 <min1125@cau.ac.kr>
2022-07-08 17:25:04 +0900

    로그아웃 기능 수정
    fix : 로그아웃 기능 수정
    
    cookie에 'scap_auth' 이름으로 저장되어 있는 토큰을 삭제하는 기능을 추가
    로그아웃 후 새로고침 시에 다시 로그인 되는 문제를 해결하기 위해 위 기능을 추가함

--- frontend/cellcraft/src/utils/cookies.js
+++ frontend/cellcraft/src/utils/cookies.js
@@ -9,8 +9,9 @@ function getAuthFromCookie () {
   )
 }
 
-function deleteCookie (value) {
-  document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+function deleteCookie () {
+//   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`
+  document.cookie = 'scap_auth=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
 }
 
 export {
