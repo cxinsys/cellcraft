9ff2d2ed56f099b9193918446d1eea85e36a663b
shindongmin11 <min1125@cau.ac.kr>
2022-07-13 16:20:39 +0900

    Update Frontend
    feat : 비주얼 프로그래밍 페이지 구현
    
    WorkFlowPage에 Drawflow로 비주얼 프로그래밍 구현
    로그인 시 WorkFlowPage로 라우팅

---
+++ frontend/cellcraft/src/components/nodes/nodeHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style>
+
+</style>
--- frontend/cellcraft/src/views/WorkFlowPage.vue
+++ frontend/cellcraft/src/views/WorkFlowPage.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-      workflow
-  </div>
+<div>
+    <button @click="exportdf">Export</button>
+    <button @click="importdf">Import</button>
+    <button @click="addnodedf">Add Node</button>
+    <div id="drawflow"></div>
+</div>
 </template>
 
 <script>
-export default {
+import Vue from 'vue'
+/* eslint-disable */
+// import Drawflow from 'drawflow'
+// import styleDrawflow from 'drawflow/dist/drawflow.min.css' // eslint-disable-line no-use-before-define
+import node from '@/components/nodes/node.vue'
 
+export default {
+  components: {
+    node
+  },
+  data () {
+    return {
+      editor: null
+    }
+  },
+  mounted () {
+    const id = document.getElementById('drawflow')
+    this.editor = new Drawflow(id, Vue)
+    this.editor.start()
+
+    this.editor.registerNode('node', node, {}, {})
+    this.editor.addNode('node', 0, 1, 150, 300, 'node', {}, 'node', 'vue')
+  },
+  methods: {
+    exportdf () {
+      this.exportValue = this.editor.export()
+      console.log(this.exportValue)
+    },
+    importdf () {
+      this.editor.import(this.exportValue)
+    },
+    addnodedf () {
+      this.editor.addNode('node', 0, 1, 150, 300, 'node', {select_type: '1'}, 'node', 'vue')
+    }
+  }
 }
 </script>
 
 <style>
-
+#drawflow {
+  width: 100%;
+  height: 800px;
+  border: 1px solid red;
+}
 </style>
