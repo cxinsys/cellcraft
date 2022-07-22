3b17e65a8e13f1da5a735be19b285d18ac41fa70
shindongmin11 <min1125@cau.ac.kr>
2022-07-22 11:54:34 +0900

    fix : workflow
    fix : workflow
    
    remove some node.vue
    add workflow.py backend
    fix node fronted

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.routes.endpoints import auth
+from app.routes.endpoints import auth, workflow
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(workflow.router, prefix="/workflow", tags=["workflow"])
