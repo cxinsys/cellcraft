5ce37ea637010d2281932c87449b171ab6c7c399
shindongmin11 <min1125@cau.ac.kr>
2022-08-17 00:56:16 +0900

    Feat : snakemake
    Feat : snakemake
    
    update workflow complie
    update snakemake

--- backend/app/routes/endpoints/workflow.py
+++ backend/app/routes/endpoints/workflow.py
@@ -1,19 +1,61 @@
 from fastapi import APIRouter, Body, Depends, HTTPException, Request, UploadFile, File
 from fastapi.encoders import jsonable_encoder
 from typing import List, Union
+from subprocess import Popen,PIPE
 import os
-
-from json import JSONDecodeError
+import json
 
 router = APIRouter()
+
+def linkList(nodeList):
+    result = []
+    #while문으로 검사
+    while True:
+        for item in nodeList:
+            for i in range(len(nodeList)):
+                if(i >= len(nodeList)):
+                    continue
+                if item[len(item)-1] == nodeList[i][0]:
+                    nodeList.append(item + nodeList[i])
+                    del nodeList[i]
+                    nodeList.remove(item)
+        break
+    for itemList in nodeList:
+        searchList = []
+        for x in itemList:
+            if x not in searchList:
+                searchList.append(x)
+            if x == itemList[len(itemList)-1]:
+                result.append(searchList)
+    return result
 
 #export workflow data
 @router.post("/export")
 async def exportData(request: Request):
     try:
         payload_as_json = await request.json()
+        inputCon_list = []
+        outputCon_list = []
+        print(payload_as_json)
+        for key, val in payload_as_json.items():
+            for val_key, val_val in val.items():
+                if val_key == "inputs":
+                    for I_val in val_val.values():
+                        inputCon_list.append([key, I_val['connections'][0]['node']])
+                elif val_key == "outputs":
+                    for O_val in val_val.values():
+                        if len(O_val['connections']) != 0:
+                            outputCon_list.append([key, O_val['connections'][0]['node']])
+                        
+        nodeObject_list = linkList(outputCon_list)
+        print(nodeObject_list)
+        # for item in nodeObject_list:
+        #     with open(f"workflow/data/build_{nodeObject_list.index(item)}.txt", 'w') as f:
+        #         f.write(payload_as_json[item[0]]["data"]["file"])
+        #     process = Popen(['snakemake',f'workflow/data/validation_{nodeObject_list.index(item)}.txt','-j'], stdout=PIPE, stderr=PIPE)
+        #     stdout, stderr = process.communicate()
         message = "success"
-    except JSONDecodeError:
+    except json.JSONDecodeError:
         payload_as_json = None
         message = "Received data is not a valid JSON"
     return {"message": message, "recived_data": payload_as_json}
