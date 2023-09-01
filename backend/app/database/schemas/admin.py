ce6ac558f3153c0d3d9f81c2cab41e7673a60912
mindong <74493250+mindongdong@users.noreply.github.com>
2023-09-01 18:09:38 +0900

    Update admin.py
    schemas files represent only data types

--- backend/app/database/schemas/admin.py
+++ backend/app/database/schemas/admin.py
@@ -1,14 +1,9 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
-from app.database.schemas.user import User
-
 class Conditions(BaseModel):
-    amount: int = 20, 
-    page_num: int = 1, 
-    sort: str = "id", 
-    order: str = "asc", 
-    searchTerm: str = None
-
-class Users(BaseModel):
-    users: list[User] = []
+    amount: int 
+    page_num: int
+    sort: str
+    order: str
+    searchTerm: Optional[str] = None
