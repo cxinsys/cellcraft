95436f4d31ed492515fe4405fd89c506b246295f
mindongdong <min1125@cau.ac.kr>
2023-09-22 00:34:18 +0900

    Improvement of the structure of snakemake and workflow

--- backend/workflow/script/FastTenet.py
+++ backend/workflow/script/FastTenet.py
@@ -14,7 +14,8 @@ def install_package(package_name, install_command):
 
 # Check and install CuPy
 # Note: Ensure that 'conda' is available in the PATH and the script has the necessary permissions.
-install_package("cupy", "conda install -c conda-forge cupy cuda-version=xx.x")  # Replace xx.x with your CUDA version
+# 12.0
+install_package("cupy", "conda install -c conda-forge cupy cuda-version=11.8")  # Replace xx.x with your CUDA version
 
 # Check and install JAX
 install_package("jax", 'pip install --upgrade pip && pip install "jax[cuda11_cudnn86]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html')
