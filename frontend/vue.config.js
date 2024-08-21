dcce2230c9109df6bcdf25401c192a06d2dae321
mindongdong <min1125@cau.ac.kr>
2024-08-21 13:17:51 +0000

    update deploy config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,26 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  runtimeCompiler: true,
-  devServer: {
-    client: {
-      overlay: false
-    }
-  },
-  css: {
-    extract: true, // CSS를 별도의 파일로 추출
-    sourceMap: false, // 소스 맵 비활성화
-    loaderOptions: {
-      css: {},
-      postcss: {},
-    },
-  },
-  configureWebpack: {
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-      },
-    },
-  },
 });
