f40a56816318be437f72cbd730cf431f00ca664a
mindongdong <min1125@cau.ac.kr>
2025-06-20 18:24:12 +0000

    chore: update Docker compose configurations and database initialization script

--- init.sql
+++ init.sql
@@ -1,13 +1,13 @@
 -- 데이터베이스 사용자 생성
--- CREATE USER cellcraft WITH PASSWORD 'cellcraft';
+CREATE USER cellcraft_admin WITH PASSWORD 'cellcraft_admin';
 
 -- 데이터베이스 생성
-CREATE DATABASE cellcraft OWNER cellcraft;
+CREATE DATABASE cellcraft OWNER cellcraft_admin;
 
 -- public 스키마 권한 부여
-GRANT USAGE ON SCHEMA public TO cellcraft;
-GRANT CREATE ON SCHEMA public TO cellcraft;
+GRANT USAGE ON SCHEMA public TO cellcraft_admin;
+GRANT CREATE ON SCHEMA public TO cellcraft_admin;
 
 -- 데이터베이스 권한 부여
-GRANT ALL PRIVILEGES ON DATABASE cellcraft TO cellcraft;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cellcraft;
+GRANT ALL PRIVILEGES ON DATABASE cellcraft TO cellcraft_admin;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cellcraft_admin;
