edffa9d27389c7125e4c14d77f11cddaee43a0eb
mindongdong <min1125@cau.ac.kr>
2026-01-13 02:06:07 +0000

    chore(frontend): bump version to v1.0.5

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -42,7 +42,7 @@
     >
       <div class="footer__content">
         <div class="copyright__txt">© 2025 CellCraft. All rights reserved</div>
-        <div class="version__info">v1.0.4 | release/v1.0.4</div>
+        <div class="version__info">v1.0.5 | release/v1.0.5</div>
       </div>
     </footer>
   </div>
